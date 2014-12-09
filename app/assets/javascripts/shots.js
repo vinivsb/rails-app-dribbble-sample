854cbd99a9b4e3c38c2eed3ffba1f9da1e63c44f
vinivsb <vinicius.vsb@gmail.com>
2014-12-09 19:39:18 -0200

    js throttle pattern added for scrolling

--- app/assets/javascripts/shots.js
+++ app/assets/javascripts/shots.js
@@ -2,6 +2,8 @@ Dribbble.Shots = (function(){
   function Shots(container) {
     this.container = container;
     this.shots = this.container.find(".shot");
+    this.timeWindow = 500
+    this.lastTime = new Date((new Date()).getTime() - this.timeWindow);
   }
  
   Shots.fn = Shots.prototype;
@@ -14,11 +16,15 @@ Dribbble.Shots = (function(){
   };
 
   Shots.fn.imagesFromVisibles = function() {
-    $.each(this.shots, function(key, shot){
-      if(verge.inViewport(shot, -30)){
-        this.show($(shot));
-      }
-    }.bind(this));
+    if ((this.lastTime.getTime() + this.timeWindow) <= (new Date()).getTime()) {
+      this.lastTime = new Date();
+      $.each(this.shots, function(key, shot){
+        if(verge.inViewport(shot, -30)){
+          this.show($(shot));
+        }
+      }.bind(this));
+    }
+    
   };
 
   Shots.fn.show = function(element) {
